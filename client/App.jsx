40819412a864daada7198f5574bbde5fde774f56
Josie Glore <josieglore@gmail.com>
2019-01-14 16:48:36 -0800

    add reducers and index.html

---
+++ client/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import Wrapper from './containers/MainContainer.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ client/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import appReducer from './appReducer';
+
+const reducers = combineReducers({
+  app: appReducer,
+});
+
+export default reducers;
