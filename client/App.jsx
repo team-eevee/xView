c2e8a991570d7296f20b562e3b0a49a7238d33dc
Josie Glore <josieglore@gmail.com>
2019-01-15 09:59:53 -0800

    add routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 // import Wrapper from './containers/MainContainer.jsx';
+import history from './../router/history.jsx'
 
 class App extends Component {
   constructor(props) {
@@ -7,9 +9,14 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>Hi</h1>
-      </div>
+      <Router history={history}>
+        <Switch>
+      
+        </Switch>
+      </Router>
+      // <div>
+      //   <h1>Hi</h1>
+      // </div>
     )
   }
 }
---
+++ client/actions/actions.js
@@ -0,0 +1,6 @@
+import * as types from '../constants/actionTypes';
+
+export const createApp = () => ({
+  type: types.CREATE_APP,
+  payload:
+})
