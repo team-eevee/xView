99c99a9d73a3a68b5ca5b9fba2fd459c867cb4be
jaelee213 <jaelee213@gmail.com>
2019-01-16 13:04:45 -0800

    added controllers for middleware in server

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -2,6 +2,7 @@ const request = require('request');
 
 // O-Auth request to Github to get authenticated user information
 const getGitHubUser = (req, res, next) => {
+	console.log('token ', req.session.access_token);
 	// request to get user information
 	request.get(
 		{
@@ -13,15 +14,14 @@ const getGitHubUser = (req, res, next) => {
 		},
 		(err, response, body) => {
 			if (err) return next(err);
-			res.locals.user = body;
+			res.locals.user = JSON.parse(body);
 		 	return next();
 		},
 	);
 };
 
-
 const getGoogleUser = (req, res, next) => {
 
 };
 
-module.exports = { getUser, };
+module.exports = { getGitHubUser, getGoogleUser };
